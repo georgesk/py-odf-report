3d1b31226a61f4470957f960cd1cd09d904aefd4
Georges Khaznadar <georgesk@debian.org>
2017-06-29 20:34:52 +0200

    modified the way images are included

--- examples/README.md
+++ examples/README.md
@@ -3,19 +3,25 @@
 Files must be of type OpenDocumentText,
 i.e. mimetype=application/vnd.oasis.opendocument.text
 
-Fields can be specified as `@@field@@` or opendocument user fields
+Fields can be specified as `@@field@@`.
 
 For images, a dummy image must be inserted in the template,
-with suitable attributes (width, height, etc.)
+with suitable attributes (width, height, etc.); then a legend
+must be appended to the image, with the field name as `@@field@@`.
 
 # Format of data to merge #
 
 Files must be in csv format. A proper sniffing is made to allow
 for various delimiters.
 
-Fields to contain image file names or image URLs **must** start
-with `img_`.
+Fields to contain image file names or image URLs will be interpreted
+as images only when a dummy image in the template document bears
+the field name in its legend (as `@@field@@`).
 
-Association between images and document are done in the order of
-appearance of image elements in the document, with no concern to the
-field name.
+# Examples #
+
+## Three pages with: given name, surname, photo ##
+
+  * data file = `examples/data.csv`
+  * template text = `examples/sample1.odt`
+
--- odfReport/__init__.py
+++ odfReport/__init__.py
@@ -23,9 +23,19 @@ from odf import opendocument, text, draw, element
 from copy import deepcopy
 from urllib import request
 from urllib.error import URLError
-import re, os.path, csv
+import re, os.path, csv, subprocess
 
 __counter=1 # counter for file names; defaults to 1 when beginning
+
+fieldNamePattern=re.compile(r"@@([^@]+)@@")
+
+djangoUrl=re.compile(
+    r'^(?:http|ftp)s?://' # http:// or https://
+    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+    r'localhost|' #localhost...
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+    r'(?::\d+)?' # optional port
+    r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
 class TemplateText:
     """
@@ -52,12 +62,12 @@ class TemplateText:
         """
         self.document=deepcopy(self.original)
         self.fields=self.findFields1()
-        self.frames=self.findFrames()
+        self.frames=self.findFrames1()
         return
     
     def findFields1(self):
         """
-        return a dictionary field name => textNode,
+        @return a dictionary field name => textNode,
         which allow one to find where to replace a field by a value.
         The syntax for fields is: @@fieldname@@
         """
@@ -68,17 +78,32 @@ class TemplateText:
                     nodes.append(n)
         result={}
         for n in nodes:
-            pattern=re.compile(r"@@([^@]+)@@")
-            found=pattern.findall(n.data)
+            found=fieldNamePattern.findall(n.data)
             for f in found:
                 result[f]=n
         return result
 
-    def findFrames(self):
+    def findFrames1(self):
         """
-        return a list of the draw:frame elements
+        Searches frames which contain one text-box child, and another
+        frame in their subtree. The attribute svg:height is born by
+        the inner frame and also as fo:min-height attribute of the
+        text-box.
+        @return a dictionary field name => (frame, width, height)
         """
-        return self.document.body.getElementsByType(draw.Frame)
+        allFrames= self.document.body.getElementsByType(draw.Frame)
+        frames=[f for f in allFrames if f.getElementsByType(draw.Frame) and f.getElementsByType(draw.TextBox)]
+        dico={}
+        for f in frames:
+            m=fieldNamePattern.match(str(f))# field name from the legend
+            if m:
+                fieldName=m.group(1)
+                innerFrame=f.getElementsByType(draw.Frame)[1] # not f itself
+                dico[fieldName]=(f,
+                             innerFrame.getAttribute("width"),
+                             innerFrame.getAttribute("height"),
+                )
+        return dico
 
     def replaceFields(self, dico={}, images=[]):
         """
@@ -93,40 +118,34 @@ class TemplateText:
                 new=str(self.fields[k]).replace("@@%s@@" %k, dico[k])
                 # replace the text data of the node
                 self.fields[k].data=new
-
-        for f,i in zip(self.frames, images):
-            # f is a draw.Frame, i is an image filename or URL
-            #
-            # use self.addPictureFromString(bytes content, string mediatype)
-            # then create a node draw:image with attributes
-            #    xlink:href pointing to the added picture,
-            #    xlink:type = "simple"
-            #    xlink:show = "embed"
-            #    xlink:actuate = "onLoad"
-            # and replace the child nodes of the frame
-            djangoUrl=re.compile(
-                r'^(?:http|ftp)s?://' # http:// or https://
-                r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
-                r'localhost|' #localhost...
-                r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
-                r'(?::\d+)?' # optional port
-                r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-            imageOK=False
-            if os.path.exists(i):
-                href=self.document.addPictureFromFile(i, mediatype="image/png")
-                imageOK=True
-            if djangoUrl.match(i):
-                try:
-                    with request.urlopen(i) as feed:
-                        mediatype=feed.info()["Content-Type"]
-                        href=self.document.addPictureFromString(feed.read(), mediatype)
-                        if mediatype.startswith("image/"):
-                            imageOK=True
-                except URLError as e:
-                    print("%s; %s not downloaded." % (e,i))
-            if imageOK:
-                f.childNodes=[]
-                f.addElement(draw.Image(href=href))
+            if k in self.frames:
+                # self.frames[k] is a tuple (frame, width, height)
+                # dico[k] is an image filename or URL
+                # use self.addPictureFromString(bytes content, string mediatype)
+                imageOK=False
+                if os.path.exists(dico[k]): # an image filename
+                    mediatype=subprocess.run("file --mime-type %s" % dico[k],
+                                             stdout=subprocess.PIPE,
+                    ).stdout
+                    href=self.document.addPictureFromFile(dico[k], mediatype=mediatype)
+                    imageOK=True
+                if djangoUrl.match(dico[k]):
+                    try:
+                        with request.urlopen(dico[k]) as feed:
+                            mediatype=feed.info()["Content-Type"]
+                            href=self.document.addPictureFromString(feed.read(), mediatype)
+                            if mediatype.startswith("image/"):
+                                imageOK=True
+                    except URLError as e:
+                        print("%s; %s not downloaded." % (e,dico[k]))
+                if imageOK:
+                    f,w,h=self.frames[k]
+                    f.setAttribute("width",w)
+                    f.setAttribute("height",h)
+                    f.childNodes=[]                     # drop all children
+                    f.addElement(draw.Image(href=href)) # insert the image
+                else:
+                    print("ERROR: bad image %s" % dico[k])
         return
 
     def save(self):
